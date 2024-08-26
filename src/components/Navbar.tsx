4dca07fd4af03c31a7a3da10a313187168c76cdf
smosco <go1ruf2tk3@gmail.com>
2024-08-26 14:07:37 +0900

    feat: Clerk 컴포넌트를 사용해 사용자의 로그인 상태에 따라 다른 UI 구현

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import MobileMenu from './MobileMenu';
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
 
 const Navbar = () => {
   return (
@@ -49,6 +56,29 @@ const Navbar = () => {
       </div>
       {/* RIGHT */}
       <div className='w-[30%] flex items-center gap-4 xl:gap-8 justify-end'>
+        <ClerkLoading>
+          <div className='inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white' />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <div className='cursor-pointer'>
+              <Image src='/people.png' alt='' width={20} height={20} />
+            </div>
+            <div className='cursor-pointer'>
+              <Image src='/messages.png' alt='' width={20} height={20} />
+            </div>
+            <div className='cursor-pointer'>
+              <Image src='/notifications.png' alt='' width={20} height={20} />
+            </div>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <div className='flex items-center gap-2 text-sm'>
+              <Image src='/login.png' alt='' width={20} height={20} />
+              <Link href='/sign-in'>Login/Register</Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
         <MobileMenu />
       </div>
     </div>

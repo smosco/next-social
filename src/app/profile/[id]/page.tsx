9bc999af47c523395082ccdfb668a48eb92fd7dd
smosco <go1ruf2tk3@gmail.com>
2024-08-26 20:01:31 +0900

    feat: Home, Profile 페이지의 LeftMenu에 type props 전달

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -6,7 +6,7 @@ const ProfilePage = () => {
   return (
     <div className='flex gap-6 pt-6'>
       <div className='hidden xl:block w-[20%]'>
-        <LeftMenu />
+        <LeftMenu type='profile' />
       </div>
       <div className='w-full lg:w-[70%] xl:w-[50%]'>
         <div className='flex flex-col gap-6'>

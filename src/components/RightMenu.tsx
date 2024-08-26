7ca25f231d48e6829f2c130725be2cd087f5832c
smosco <go1ruf2tk3@gmail.com>
2024-08-26 18:04:09 +0900

    feat: RightMenu 컴포넌트 기본 UI 레이아웃 구성

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,5 +1,15 @@
-const RightMenu = () => {
-  return <div>RightMenu</div>;
+import Birthday from './Birthday';
+import FriendRequests from './FriendRequests';
+import Ad from './Ad';
+
+const RightMenu = ({ userId }: { userId?: string }) => {
+  return (
+    <div className='flex flex-col gap-6'>
+      <FriendRequests />
+      <Birthday />
+      <Ad size='md' />
+    </div>
+  );
 };
 
 export default RightMenu;

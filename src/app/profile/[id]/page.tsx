11933d0581572f71c58d6652ef93f84db8118990
smosco <go1ruf2tk3@gmail.com>
2024-08-26 19:16:58 +0900

    feat: Profile 페이지 구현

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,23 @@
+import LeftMenu from '@/components/LeftMenu';
+import RightMenu from '@/components/RightMenu';
+import Feed from '@/components/Feed';
+
+const ProfilePage = () => {
+  return (
+    <div className='flex gap-6 pt-6'>
+      <div className='hidden xl:block w-[20%]'>
+        <LeftMenu />
+      </div>
+      <div className='w-full lg:w-[70%] xl:w-[50%]'>
+        <div className='flex flex-col gap-6'>
+          <Feed />
+        </div>
+      </div>
+      <div className='hidden lg:block w-[30%]'>
+        <RightMenu userId='test' />
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;

c8a482ec66e4f38de2ac0c2d114fc74f3c00db23
smosco <go1ruf2tk3@gmail.com>
2024-08-26 16:42:11 +0900

    feat: Post에 Comment 추가

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Comments from './Comments';
 
 const Post = () => {
   return (
@@ -80,6 +81,7 @@ const Post = () => {
           </div>
         </div>
       </div>
+      <Comments />
     </div>
   );
 };

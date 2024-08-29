4714de813a91a9f0c421bb9616c124567aa69786
smosco <go1ruf2tk3@gmail.com>
2024-08-29 16:32:05 +0900

    feat: 특정 사용자 게시물 및 팔로우한 사용자의 게시물 조회 로직 추가

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,14 +1,84 @@
+import { auth } from '@clerk/nextjs/server';
 import Post from './Post';
+import prisma from '@/lib/client';
 
-const Feed = () => {
+const Feed = async ({ username }: { username?: string }) => {
+  const { userId } = auth();
+
+  let posts: any[] = [];
+
+  // 특정 사용자의 게시물 (username이 제공된 경우)
+  if (username) {
+    posts = await prisma.post.findMany({
+      where: {
+        user: {
+          username: username,
+        },
+      },
+      include: {
+        user: true,
+        likes: {
+          select: {
+            userId: true,
+          },
+        },
+        _count: {
+          select: {
+            comments: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+
+  // 로그인 사용자가 팔로우하는 사람들의 게시물 (username이 제공되지 않은 경우)
+  if (!username && userId) {
+    const following = await prisma.follower.findMany({
+      where: {
+        followerId: userId,
+      },
+      select: {
+        followingId: true,
+      },
+    });
+
+    const followingIds = following?.map((f) => f.followingId);
+
+    posts = await prisma.post.findMany({
+      where: {
+        userId: {
+          in: followingIds,
+        },
+      },
+      include: {
+        user: true,
+        likes: {
+          select: {
+            userId: true,
+          },
+        },
+        _count: {
+          select: {
+            comments: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+
   return (
     <div className='p-4 bg-white shadow-md rounded-lg flex flex-col gap-12'>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <div className='p-4 bg-white shadow-md rounded-lg flex flex-col gap-12'>
+        {posts.length
+          ? posts.map((post) => <Post key={post.id} post={post} />)
+          : 'No posts found!'}
+      </div>
     </div>
   );
 };
--- src/components/feed/Post.tsx
+++ src/components/feed/Post.tsx
@@ -1,39 +1,48 @@
 import Image from 'next/image';
 import Comments from './Comments';
+import { User, Post as PostType } from '@prisma/client';
 
-const Post = () => {
+type FeedPostType = PostType & { user: User } & {
+  likes: { userId: string };
+} & {
+  _count: { comments: number };
+};
+
+const Post = ({ post }: { post: FeedPostType }) => {
   return (
     <div className='flex flex-col gap-4'>
       {/* USER */}
       <div className='flex items-center justify-between'>
         <div className='flex items-center gap-4'>
           <Image
-            src='https://images.pexels.com/photos/161853/eiffel-tower-paris-france-tower-161853.jpeg?auto=compress&cs=tinysrgb&w=300'
+            src={post.user.avatar || '/noAvatar.png'}
             alt=''
             width={40}
             height={40}
             className='w-10 h-10 rounded-full'
           />
-          <span className='font-medium'>Jean</span>
+          <span className='font-medium'>
+            {post.user.name && post.user.surname
+              ? `${post.user.name} ${post.user.surname}`
+              : post.user.username}
+          </span>
         </div>
         <Image src='/more.png' alt='' width={16} height={16} />
       </div>
       {/* DESC */}
       <div className='flex flex-col gpa-4 '>
-        <div className='w-full min-h-96 relative'>
-          <Image
-            src='https://images.pexels.com/photos/3073666/pexels-photo-3073666.jpeg?auto=compress&cs=tinysrgb&w=300'
-            alt=''
-            fill
-            className='object-cover rounded-md'
-          />
-        </div>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id enim
-          eveniet molestias nisi. Maxime iusto facilis architecto beatae
-          quisquam laboriosam dolore qui mollitia porro, vero animi earum? Error
-          unde, nulla ducimus quo quas neque vero.
-        </p>
+        {post.img && (
+          <div className='w-full min-h-96 relative'>
+            <Image
+              src={post.img}
+              alt=''
+              fill
+              className='object-cover rounded-md'
+            />
+          </div>
+        )}
+
+        <p>{post.desc}</p>
       </div>
       {/* INTERACTION */}
       <div className='flex items-center justify-between text-sm my-4'>
@@ -76,7 +85,7 @@ const Post = () => {
             />
             <span className='text-gray-300'>|</span>
             <span className='text-gray-500'>
-              123<span className='hidden md:inline'> Shares</span>
+              123<span className='hidden md:inline'> Share</span>
             </span>
           </div>
         </div>

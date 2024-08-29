bfeb5ec60943f2c09f39a6f9292b37a2c18dffe0
smosco <go1ruf2tk3@gmail.com>
2024-08-29 11:56:54 +0900

    feat: Zod를 사용한 유저 프로필 업데이트 폼의 정보 유효성 검사 및 업데이트 기능 구현

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -2,6 +2,7 @@
 
 import { auth } from '@clerk/nextjs/server';
 import prisma from './client';
+import { z } from 'zod';
 
 export const switchFollow = async (userId: string) => {
   const { userId: currentUserId } = auth();
@@ -148,5 +149,48 @@ export const declineFollowRequest = async (userId: string) => {
   } catch (err) {
     console.log(err);
     throw new Error('Something went wrong!');
+  }
+};
+
+export const updateProfile = async (formData: FormData) => {
+  const fields = Object.fromEntries(formData);
+
+  const filteredFields = Object.fromEntries(
+    Object.entries(fields).filter(([_, value]) => value !== '')
+  );
+
+  const Profile = z.object({
+    cover: z.string().optional(),
+    name: z.string().max(60).optional(),
+    surname: z.string().max(60).optional(),
+    description: z.string().max(255).optional(),
+    city: z.string().max(60).optional(),
+    school: z.string().max(60).optional(),
+    work: z.string().max(60).optional(),
+    website: z.string().max(60).optional(),
+  });
+
+  const validatedFields = Profile.safeParse(filteredFields);
+
+  if (!validatedFields.success) {
+    console.log(validatedFields.error.flatten().fieldErrors);
+    return 'err';
+  }
+
+  const { userId } = auth();
+
+  if (!userId) {
+    return 'err';
+  }
+
+  try {
+    await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: validatedFields.data,
+    });
+  } catch (err) {
+    console.log(err);
   }
 };
